e9d023c2d516b04a9601764bae1a2360790da620
M1053737 <nagendra.ca2@mindtree.com>
2019-09-06 12:00:59 +0530

    first

---
+++ Helloworld/src/SPD/Hello.java
@@ -0,0 +1,14 @@
+package SPD;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Helloworld");
+		
+		System.out.println("nagis");
+		System.out.println("Backkkkk");
+		System.out.println("dsds");
+		System.out.println("go to hell");
+	}
+
+}
